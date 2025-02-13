b9b32b70b54f7e1cf23cd7c3ee47b64b50601878
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-13 00:13:13 -0600

    Add initial files

---
+++ src/js/virtual_crossing.js
@@ -0,0 +1,11 @@
+const API_KEY = 'F2DHRQD5ABQFKKKTJZRRL72TK';
+
+class virtualCrossingInterface {
+    constructor(win, doc) {
+	this.win = win;
+    }
+
+    fetchLocationWeather(location) {
+	
+    }
+}
