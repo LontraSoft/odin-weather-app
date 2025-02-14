3af13897c34f6ae5035201dae23dd944fce11850
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-13 23:09:25 -0600

    Add update functionality on search

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,42 @@ import VisualCrossingInterface from './virtual_crossing';
 
 let visualCrossing = new VisualCrossingInterface(window);
 
+function updateWeatherConditions(conditions) {
+    const weatherConditionsNode = document.querySelector(`.${HTML_CONSTANTS.CONDITION_TEXT_CLASS}`);
+    weatherConditionsNode.textContent = conditions;
+}
+
+function updateTemperature(temperatureData, feelsLikeData) {
+    const minTemperatureNode = document.querySelector(`.${HTML_CONSTANTS.TEMPERATURE_MIN_TEXT_CLASS}`);
+    const temperatureNode = document.querySelector(`.${HTML_CONSTANTS.TEMPERATURE_TEXT_CLASS}`);
+    const maxTemperatureNode = document.querySelector(`.${HTML_CONSTANTS.TEMPERATURE_MAX_TEXT_CLASS}`);
+    const minFeelsLikeNode = document.querySelector(`.${HTML_CONSTANTS.FEELS_LIKE_MIN_TEXT_CLASS}`);
+    const feelsLikeNode = document.querySelector(`.${HTML_CONSTANTS.FEELS_LIKE_TEXT_CLASS}`);
+    const maxFeelsLikeNode = document.querySelector(`.${HTML_CONSTANTS.FEELS_LIKE_MAX_TEXT_CLASS}`);
+
+    minTemperatureNode.textContent = temperatureData.min;
+    temperatureNode.textContent = temperatureData.current;
+    maxTemperatureNode.textContent = temperatureData.max;
+    minFeelsLikeNode.textContent = feelsLikeData.min;
+    feelsLikeNode.textContent = feelsLikeData.current;
+    maxFeelsLikeNode.textContent = feelsLikeData.max;
+}
+
+function updateHumidity(humidity) {
+    const humidityNode = document.querySelector(`.${HTML_CONSTANTS.HUMIDITY_TEXT_CLASS}`);
+    humidityNode.textContent = humidity;
+}
+
+function updateWind(windSpeed) {
+    const windNode = document.querySelector(`.${HTML_CONSTANTS.WIND_TEXT_CLASS}`);
+    windNode.textContent = windSpeed;
+}
+
 function updateWeather(weatherData) {
-    console.log('Updating weather');
-    console.log(weatherData);
+    updateWeatherConditions(weatherData.conditions);
+    updateTemperature(weatherData.temperature, weatherData.feelsLike);
+    updateHumidity(weatherData.humidity);
+    updateWind(weatherData.windSpeed);
 }
 
 function submitWeatherSearch(event) {
