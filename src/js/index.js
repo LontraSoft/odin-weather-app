d2363f81483a7f2014b4dac79e297fac6b21f612
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-13 22:48:35 -0600

    Add submit function

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,22 @@ import VisualCrossingInterface from './virtual_crossing';
 
 let visualCrossing = new VisualCrossingInterface(window);
 
+function updateWeather(weatherData) {
+    console.log('Updating weather');
+    console.log(weatherData);
+}
+
+function submitWeatherSearch(event) {
+    event.preventDefault();
+    
+    const locationInputNode = document.getElementById(HTML_CONSTANTS.LOCATION_INPUT_ID);
+    const location = locationInputNode.value;
+
+    visualCrossing.fetchLocationWeather(location).then(res => {
+	updateWeather(res);
     });
+}
+
 function attachEventHandlers() {
     const searchButtonNode = document.getElementById(HTML_CONSTANTS.LOCATION_SUBMIT_ID);
     
