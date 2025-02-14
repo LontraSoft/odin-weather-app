3c401de121347552cd3aa1da6f8647ebdfa8b29f
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-13 22:44:38 -0600

    Add html constants

---
+++ src/js/html_constants.js
@@ -0,0 +1,20 @@
+export const CONDITION_SECTION_CLASS = 'condition-section';
+export const CONDITION_TEXT_CLASS = 'condition-text';
+
+export const TEMPERATURE_CONTAINER_CLASS = 'temperature-container';
+export const TEMPERATURE_MIN_TEXT_CLASS = 'temp-min';
+export const TEMPERATURE_TEXT_CLASS = 'temp-current';
+export const TEMPERATURE_MAX_TEXT_CLASS = 'temp-max';
+export const FEELS_LIKE_MIN_TEXT_CLASS = 'feels-min';
+export const FEELS_LIKE_TEXT_CLASS = 'feels-current';
+export const FEELS_LIKE_MAX_TEXT_CLASS = 'feels-max';
+
+export const HUMIDITY_CONTAINER_CLASS = 'humidity-container';
+export const HUMIDITY_TEXT_CLASS = 'humidity-text';
+
+export const WIND_CONTAINER_CLASS = 'wind-container';
+export const WIND_TEXT_CLASS = 'wind-text';
+
+export const LOCATION_INPUT_ID = 'location-search';
+export const LOCATION_SUBMIT_ID = 'search-btn';
+
