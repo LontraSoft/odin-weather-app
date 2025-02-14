8ad6f3139d80e7063d241f31bc788d64de438bf4
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-13 23:40:23 -0600

    Rename containers to sections to match html

--- src/js/html_constants.js
+++ src/js/html_constants.js
@@ -1,7 +1,7 @@
 export const CONDITION_SECTION_CLASS = 'condition-section';
 export const CONDITION_TEXT_CLASS = 'condition-text';
 
-export const TEMPERATURE_CONTAINER_CLASS = 'temperature-container';
+export const TEMPERATURE_CONTAINER_CLASS = 'temperature-section';
 export const TEMPERATURE_MIN_TEXT_CLASS = 'temp-min';
 export const TEMPERATURE_TEXT_CLASS = 'temp-current';
 export const TEMPERATURE_MAX_TEXT_CLASS = 'temp-max';
@@ -9,10 +9,10 @@ export const FEELS_LIKE_MIN_TEXT_CLASS = 'feels-min';
 export const FEELS_LIKE_TEXT_CLASS = 'feels-current';
 export const FEELS_LIKE_MAX_TEXT_CLASS = 'feels-max';
 
-export const HUMIDITY_CONTAINER_CLASS = 'humidity-container';
+export const HUMIDITY_CONTAINER_CLASS = 'humidity-section';
 export const HUMIDITY_TEXT_CLASS = 'humidity-text';
 
-export const WIND_CONTAINER_CLASS = 'wind-container';
+export const WIND_CONTAINER_CLASS = 'wind-section';
 export const WIND_TEXT_CLASS = 'wind-text';
 
 export const LOCATION_INPUT_ID = 'location-search';
